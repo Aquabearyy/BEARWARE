3ac69d37fce2997ca9d0be034b56507d28a5adc8
sxlent404 <aquabeary@outlook.com>
2024-11-19 17:35:19 +0200

    Update rivals.lua

--- Others/rivals.lua
+++ Others/rivals.lua
@@ -2,7 +2,7 @@ if not game:IsLoaded() then
     game.Loaded:Wait()
 end
 
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/sxlent404/ModdedOrion/refs/heads/main/orion.lua'))()
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
 local LocalPlayer = Players.LocalPlayer
