b0a179fc650d9e6be1a512145631f34771f4ec56
sxlent404 <aquabeary@outlook.com>
2024-12-29 12:16:54 +0200

    slapbattles.lua

--- Games/Slap Battles/slapbattles.lua
+++ Games/Slap Battles/slapbattles.lua
@@ -2,7 +2,7 @@ if not game:IsLoaded() then
     game.Loaded:Wait()
 end
 
-local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/sxlent404/ModdedOrion/refs/heads/main/orion.lua'))()
+local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/sxlent404/ModdedOrion/main/source.lua'))()
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local RunService = game:GetService("RunService")
